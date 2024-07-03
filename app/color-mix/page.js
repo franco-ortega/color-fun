f27f8b20663e75de9890b04345e49f81c854be30
Franco Ortega <francopdx@gmail.com>
2024-07-03 15:37:48 -0700

    set up initial color mixga

--- app/color-mix/page.js
+++ app/color-mix/page.js
@@ -2,11 +2,11 @@ import styles from './page.module.css';
 
 export default function ColorMix() {
 	return (
-		<main>
+		<main className={styles['color-mix']}>
 			<header>
 				<h1>Color Mix</h1>
 			</header>
-			<div className={styles.color}>
+			<div className={styles.colors}>
 				<div></div>
 				<div></div>
 				<div></div>
