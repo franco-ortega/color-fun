7364f6b36ee08f945e2c25f2dfdc02cb300a3f7f
Franco Ortega <francopdx@gmail.com>
2024-07-03 17:08:08 -0700

    rename gradient page component to Gradient

--- app/color-mix/gradient/page.js
+++ app/color-mix/gradient/page.js
@@ -1,6 +1,6 @@
 import styles from './page.module.css';
 
-export default function ColorMix() {
+export default function Gradient() {
 	return (
 		<main className={styles['color-mix']}>
 			<header>
