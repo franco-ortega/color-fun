f739ec35e2ea3a2b3a190c7b3a7634a6df38e222
Franco Ortega <francopdx@gmail.com>
2024-07-03 18:05:03 -0700

    move ColorMix component into its own folder

--- app/color-mix/layout.js
+++ app/color-mix/layout.js
@@ -1,5 +1,5 @@
 import { Inter } from 'next/font/google';
-import ColorMix from '../components/ColorMix';
+import ColorMix from '../components/colorMix/ColorMix';
 
 const inter = Inter({ subsets: ['latin'] });
 
