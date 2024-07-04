f6c0c359be4c8546076dcbeaf94d4db1028ff46c
Franco Ortega <francopdx@gmail.com>
2024-07-03 17:07:28 -0700

    create separate page for Brand fun

---
+++ app/color-mix/brand/page.js
@@ -0,0 +1,19 @@
+import styles from './page.module.css';
+
+export default function Brand() {
+	return (
+		<main className={styles['color-mix']}>
+			<header>
+				<h1>Brand Stuff</h1>
+			</header>
+			<section className={styles.brand}>
+				<div>
+					Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro harum,
+					perferendis facilis veniam ipsum repudiandae accusantium pariatur
+					minus optio in quis minima eligendi nam quaerat atque, perspiciatis
+					dolor illum beatae!
+				</div>
+			</section>
+		</main>
+	);
+}
