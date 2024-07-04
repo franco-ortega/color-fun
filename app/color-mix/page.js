d7be8e0d2a3368526cb4d3ce1d009febef8d4816
Franco Ortega <francopdx@gmail.com>
2024-07-03 17:31:11 -0700

    create orb for Gradient in color mix page

--- app/color-mix/page.js
+++ app/color-mix/page.js
@@ -2,32 +2,11 @@ import styles from './page.module.css';
 
 export default function ColorMix() {
 	return (
-		<main className={styles['color-mix']}>
-			<header>
-				<h1>Color Mix</h1>
-			</header>
-			<section className={styles.colors}>
-				<div></div>
-				<div></div>
-				<div></div>
-				<div></div>
-				<div></div>
-				<div></div>
-				<div></div>
-				<div></div>
-				<div></div>
-			</section>
-			<section className={styles.brand}>
-				<header>
-					<h2>Brand Stuff</h2>
-				</header>
-				<div>
-					Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro harum,
-					perferendis facilis veniam ipsum repudiandae accusantium pariatur
-					minus optio in quis minima eligendi nam quaerat atque, perspiciatis
-					dolor illum beatae!
-				</div>
-			</section>
-		</main>
+		<nav className={styles.colorMix}>
+			<ul>
+				<li data='gradient'>Gradient</li>
+				<li>Brand</li>
+			</ul>
+		</nav>
 	);
 }
