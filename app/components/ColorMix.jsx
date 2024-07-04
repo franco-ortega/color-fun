53739a96992faaad1c2dda5371742eb95f488cac
Franco Ortega <francopdx@gmail.com>
2024-07-03 18:00:39 -0700

    access page subtitle via path

--- app/components/ColorMix.jsx
+++ app/components/ColorMix.jsx
@@ -1,10 +1,20 @@
+'use client';
+
+import { usePathname } from 'next/navigation';
 import styles from './ColorMix.module.css';
 
 export default function ColorMix({ children }) {
+	const pathname = usePathname();
+
+	// const page = pathname.slice(11);
+	const page = pathname.slice(11, 12).toUpperCase() + pathname.slice(12);
+
+	const subheader = page ? `: ${page}` : null;
+
 	return (
 		<div className={styles.ColorMix}>
 			<header>
-				<h1>Color Mix</h1>
+				<h1>Color Mix{subheader}</h1>
 			</header>
 			<main>{children}</main>
 		</div>
