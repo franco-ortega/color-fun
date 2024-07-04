05cbcac26bdf17268e50b2db7ae51134426f6465
Franco Ortega <francopdx@gmail.com>
2024-07-03 17:04:28 -0700

    create separate page for Gradient fun

---
+++ app/color-mix/gradient/page.js
@@ -0,0 +1,22 @@
+import styles from './page.module.css';
+
+export default function ColorMix() {
+	return (
+		<main className={styles['color-mix']}>
+			<header>
+				<h1>Gradient</h1>
+			</header>
+			<div className={styles.colors}>
+				<div></div>
+				<div></div>
+				<div></div>
+				<div></div>
+				<div></div>
+				<div></div>
+				<div></div>
+				<div></div>
+				<div></div>
+			</div>
+		</main>
+	);
+}
