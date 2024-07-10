7e43e5a4487c26d75438ccf69d6962471785d474
Franco Ortega <francopdx@gmail.com>
2024-07-09 17:37:05 -0700

    clean up Home component after layout and header refactor

--- components/home/Home.jsx
+++ components/home/Home.jsx
@@ -1,31 +1,9 @@
-import Link from 'next/link';
 import styles from './Home.module.css';
-import ButtonLink from '../buttons/ButtonLink';
-import Header from '../header/Header';
 
 export default function Home() {
 	return (
-		<div className={styles.Home}>
-			{/* <header>
-				<h1>Color Playground</h1>
-				<nav>
-					<ul>
-						<li>
-							<ButtonLink text={'HOME'} url={'/'} />
-						</li>
-						<li>
-							<ButtonLink text={'COLOR MIX'} url={'/color-mix'} />
-						</li>
-						<li>
-							<ButtonLink text={'PROGRESS BAR'} url={'/progress-bar'} />
-						</li>
-					</ul>
-				</nav>
-			</header> */}
-			{/* <Header /> */}
-			<main>
-				<p>A playground to test color styles.</p>
-			</main>
-		</div>
+		<main className={styles.Home}>
+			<p>A playground to test color styles.</p>
+		</main>
 	);
 }
