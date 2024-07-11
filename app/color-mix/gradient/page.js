b99de7fe6bfc6f87ad79eaeecb7161ad9808c31b
Franco Ortega <francopdx@gmail.com>
2024-07-11 15:00:56 -0700

    make Gradient page more responsive in terms of color swath size and keep it in its container

--- app/color-mix/gradient/page.js
+++ app/color-mix/gradient/page.js
@@ -1,19 +1,19 @@
 import styles from './page.module.css';
 
 export default function Gradient() {
-	return (
-		<div className={styles['color-mix']}>
-			<div className={styles.colors}>
-				<div></div>
-				<div></div>
-				<div></div>
-				<div></div>
-				<div></div>
-				<div></div>
-				<div></div>
-				<div></div>
-				<div></div>
-			</div>
-		</div>
+	const colorSwaths = (
+		<>
+			<div></div>
+			<div></div>
+			<div></div>
+			<div></div>
+			<div></div>
+			<div></div>
+			<div></div>
+			<div></div>
+			<div></div>
+		</>
 	);
+
+	return <div className={styles.colorMix}>{colorSwaths}</div>;
 }
