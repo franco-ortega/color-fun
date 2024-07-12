e3df64bfa5e71be0a35e0eeb94bb8426aee31d3f
Franco Ortega <francopdx@gmail.com>
2024-07-12 16:52:15 -0700

    replace REPLACE methods with ternary inside MAP method

--- utils/createSubtitle.js
+++ utils/createSubtitle.js
@@ -1,11 +1,15 @@
 export default function createSubtitle(path) {
 	return path
 		.substring(1)
-		.replace('-', ' ')
-		.replace('/', ' > ')
 		.split('')
 		.map((character, i, list) =>
-			list[i - 1] === undefined || list[i - 1] === ' '
+			character === '-'
+				? ' '
+				: character === '/'
+				? ' > '
+				: list[i - 1] === undefined ||
+				  list[i - 1] === '-' ||
+				  list[i - 1] === '/'
 				? character.toUpperCase()
 				: character
 		)
