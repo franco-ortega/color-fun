d8893e10561db2f6b74512464ada6321387ea90e
Franco Ortega <francopdx@gmail.com>
2024-07-12 15:12:11 -0700

    display page subtitle in header

--- components/header/Header.jsx
+++ components/header/Header.jsx
@@ -1,11 +1,32 @@
+'use client';
+
+import { usePathname } from 'next/navigation';
 import ButtonLink from '../buttons/ButtonLink';
 import styles from './Header.module.css';
 
 function Header() {
+	const pathname = usePathname();
+
+	const page = pathname
+		.substring(1)
+		.replace('-', ' ')
+		.replace('/', ' > ')
+		.split('')
+		.map((character, i, arr) =>
+			arr[i - 1] === undefined || arr[i - 1] === ' '
+				? character.toUpperCase()
+				: character
+		)
+		.join('');
+
+	const subtitle = page ? `: ${page}` : null;
+
+	console.log('PATHNAME', pathname);
+
 	return (
 		<header className={styles.Header}>
 			<div>
-				<h1>Color Playground</h1>
+				<h1>Color Playground{subtitle && subtitle}</h1>
 			</div>
 			<nav>
 				<ul>
