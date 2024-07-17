9cdb3e575f7e7ab88e5a64aa82f567d0ca6fa2e5
Franco Ortega <francopdx@gmail.com>
2024-07-17 15:05:12 -0700

    create initial ProgressBar component

---
+++ components/progressBar/ProgressBar.jsx
@@ -0,0 +1,13 @@
+import styles from './ProgressBar.module.css';
+
+function ProgressBar() {
+	return (
+		<div className={styles.ProgressBar}>
+			<h2>ProgressBar</h2>
+			<div>
+				<div></div>
+			</div>
+		</div>
+	);
+}
+export default ProgressBar;
