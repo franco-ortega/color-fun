31cc43f954be69572d75c14e77e34084e81ca8b7
Franco Ortega <francopdx@gmail.com>
2024-07-17 15:10:53 -0700

    create state to hold value of input

--- components/progressBar/ProgressBar.jsx
+++ components/progressBar/ProgressBar.jsx
@@ -1,6 +1,16 @@
+'use client';
+import { useState } from 'react';
 import styles from './ProgressBar.module.css';
 
 function ProgressBar() {
+	const [progress, setProgress] = useState(0);
+
+	function onProgressChange(e) {
+		setProgress(e.target.value);
+	}
+
+	console.log(progress);
+
 	return (
 		<div className={styles.ProgressBar}>
 			<h2>ProgressBar</h2>
@@ -8,7 +18,7 @@ function ProgressBar() {
 				<div></div>
 			</div>
 			<div>
-				<input type='number' />
+				<input onChange={onProgressChange} type='number' />
 			</div>
 		</div>
 	);
