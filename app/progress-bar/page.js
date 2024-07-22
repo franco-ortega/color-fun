a6eaa8e30b72e54c0e2ed5a466b53c40da9dd299
Franco Ortega <francopdx@gmail.com>
2024-07-22 16:51:50 -0700

    transfer Heading and Adjust Progress logic into progress-bar page

--- app/progress-bar/page.js
+++ app/progress-bar/page.js
@@ -1,10 +1,53 @@
+'use client';
+
 import Link from 'next/link';
 import styles from './page.module.css';
 import ProgressBar from '@/components/progressBar/ProgressBar';
+import { useState } from 'react';
 
 export default function ProgressBarPage() {
+	const [progress, setProgress] = useState(0);
+
+	function onProgressChange(e) {
+		if (e.target.value <= 100) setProgress(Number(e.target.value));
+	}
+
+	function onProgressIncrease() {
+		if (progress < 100) setProgress((prev) => Number(prev) + 1);
+	}
+
+	function onProgressDecrease() {
+		if (progress > 0) setProgress((prev) => Number(prev) - 1);
+	}
 	return (
-		<main>
+		<main className={styles.main}>
+			<h2>Progress Bar</h2>
+			{/* <div>
+				<p>{`${progress}% complete`}</p>
+				<div>
+					<div>
+						<div style={{ width: `${progress}%` }}></div>
+					</div>
+				</div>
+			</div> */}
+
+			<div className={styles.adjust}>
+				<p>ADJUST PROGRESS</p>
+				<div>
+					<button onClick={onProgressIncrease}>⬆︎</button>
+					<label htmlFor='progress-bar'>
+						<input
+							id='progress-bar'
+							onChange={onProgressChange}
+							type='number'
+							value={progress}
+							max={100}
+							min={0}
+						/>
+					</label>
+					<button onClick={onProgressDecrease}>⬇︎</button>
+				</div>
+			</div>
 			<ProgressBar />
 		</main>
 	);
