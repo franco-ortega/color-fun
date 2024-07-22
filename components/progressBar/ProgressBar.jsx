fce62475e0555c8c9e92c57a3c05f9bf2b5f7a6e
Franco Ortega <francopdx@gmail.com>
2024-07-22 16:57:47 -0700

    remove repetitive heading from ProgressBar

--- components/progressBar/ProgressBar.jsx
+++ components/progressBar/ProgressBar.jsx
@@ -3,7 +3,6 @@ import styles from './ProgressBar.module.css';
 function ProgressBar({ progress }) {
 	return (
 		<div className={styles.ProgressBar}>
-			<h2>Progress Bar</h2>
 			<div>
 				<p>{`${progress}% complete`}</p>
 				<div>
