ac8a1fa9527bd14b03f0f7a7c6e29db3f10d4b03
Franco Ortega <francopdx@gmail.com>
2024-07-23 16:36:13 -0700

    create ProjectAdjuster to adjust progress separately for each progress bar size

--- app/progress-bar/page.js
+++ app/progress-bar/page.js
@@ -1,30 +1,32 @@
 'use client';
 
 import useValue from '@/hooks/useValue';
+import ProgressAdjuster from '@/components/progressAdjuster/ProgressAdjuster';
 import ProgressBar from '@/components/progressBar/ProgressBar';
 import styles from './page.module.css';
 
 export default function ProgressBarPage() {
 	const { onValueChange, onValueDecrease, onValueIncrease, value } = useValue();
+	const sizes = [
+		{ id: 1, size: 'small' },
+		{ id: 2, size: 'medium' },
+		{ id: 3, size: 'large' },
+	];
 
 	return (
 		<main className={styles.main}>
 			<h2>Progress Bar</h2>
 
-			<div>
-				<p>{`${value}% complete`}</p>
-				<ProgressBar size={'small'} value={value} />
-			</div>
+			<ProgressAdjuster size={'small'} value={value} />
+			<ProgressAdjuster size={'medium'} value={value} />
+			<ProgressAdjuster size={'large'} value={value} />
 
-			<div>
-				<p>{`${value}% complete`}</p>
-				<ProgressBar size={'medium'} value={value} />
-			</div>
-
-			<div>
-				<p>{`${value}% complete`}</p>
-				<ProgressBar size={'large'} value={value} />
-			</div>
+			{sizes.map(({ id, size }) => (
+				<div key={id}>
+					<p>{`${value}% complete`}</p>
+					<ProgressBar size={size} value={value} />
+				</div>
+			))}
 
 			<div className={styles.adjust}>
 				<p>ADJUST PROGRESS</p>
---
+++ components/progressAdjuster/ProgressAdjuster.jsx
@@ -0,0 +1,34 @@
+import useValue from '@/hooks/useValue';
+import ProgressBar from '../progressBar/ProgressBar';
+import styles from './ProgressAdjuster.module.css';
+
+export default function ProgressAdjuster({ size }) {
+	const { onValueChange, onValueDecrease, onValueIncrease, value } = useValue();
+
+	return (
+		<div className={styles.ProgressAdjuster}>
+			<div>
+				<p>{`${value}% complete`}</p>
+				<ProgressBar size={size} value={value} />
+			</div>
+			<div>
+				<p>ADJUST PROGRESS</p>
+				<div>
+					<button onClick={onValueDecrease}>⬇︎</button>
+
+					<label htmlFor='progress-bar'>
+						<input
+							id='progress-bar'
+							onChange={onValueChange}
+							type='number'
+							value={value}
+							max={100}
+							min={0}
+						/>
+					</label>
+					<button onClick={onValueIncrease}>⬆︎</button>
+				</div>
+			</div>
+		</div>
+	);
+}
