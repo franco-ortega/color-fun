6b1fa262855c4e879396f3e3aef49f5c9534ed6b
Franco Ortega <francopdx@gmail.com>
2024-07-22 17:08:56 -0700

    display progress and update progress bar colors

--- components/progressBar/ProgressBar.jsx
+++ components/progressBar/ProgressBar.jsx
@@ -3,12 +3,10 @@ import styles from './ProgressBar.module.css';
 function ProgressBar({ progress }) {
 	return (
 		<div className={styles.ProgressBar}>
+			<p>{`${progress}% complete`}</p>
 			<div>
-				<p>{`${progress}% complete`}</p>
 				<div>
-					<div>
-						<div style={{ width: `${progress}%` }}></div>
-					</div>
+					<div style={{ width: `${progress}%` }}></div>
 				</div>
 			</div>
 		</div>
